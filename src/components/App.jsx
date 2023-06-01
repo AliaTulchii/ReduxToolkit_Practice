c064ac287d4f344ab90620a1735d48a26669750c
Halina Tulchii <halendeihalyna@gmail.com>
2023-06-01 14:05:53 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import TodoItem from './TodoItem';
 import User from './User'
 import Form from './Form'
-import Posts from './Posts';
 import css from './App.module.css'
 import { useSelector } from 'react-redux';
 
@@ -13,24 +12,19 @@ function App() {
   return (
     <div className={css.App}>
       <div className='container mx-auto px-4'>
-              <header className='flex gap-20 '>
+              <header className='flex content-center gap-20 '>
                   <div className='w-1/3'>
                       <h1 className='font-bold my-5'>Redux Toolkit State Change</h1>
                       <User />
                   </div>
                   <div className='w-1/3'>
                       <h1 className='font-bold my-5'>Redux Toolkit Todo App</h1>
-                      <Form />
-            
-            
+                      <Form />        
+                      
                       {todos?.map(todo => <TodoItem key={todo.id} todo={todo} />)}
                   </div>
-                  <div className='w-1/3'>
-                      <h1 className='font-bold my-5'>Redux Toolkit Async Thunk</h1>
-                      <Posts />
-                  </div>
               </header>
-          </div>
+      </div>
     </div>
   );
 
